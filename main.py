59443190ab1a96b4c839a3f52d5a291da799ba35
heeyoonj <jhy156250@gmail.com>
2024-04-18 11:26:06 +0000

    sum_of_squares_of_even finished

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Returns:
 A list of even integers.
 """
 # TODO: Implement even_list
-pass
+	return [num for num in int_list if num % 2 == 0]
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
@@ -22,15 +22,15 @@ Returns:
 The sum of the squares of all even numbers in the list.
 """
 # TODO: Implement sum_of_squares_of_even
-pass
+	return sum(num ** 2 for num in even_int_list if num % 2 == 0)
 
 # Main function
 def main():
-# Example list
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-even_int_list = even_list(int_list)
-output = sum_of_squares_of_even(even_int_list)
-print(output)
+	input_list = list(map(int, input("Input: ").strip().split()))
+	even_numbers = even_list(input_list)
+	sum_squares_even = sum_of_squares_of_even(even_numbers)
+	print(f"Output: {sum_squares_even}")
+
 # Boilerplate code
 if __name__ == "__main__":
 main()
